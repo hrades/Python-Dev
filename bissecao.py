862eba6d49fea50c5227550004261cd9876ce8a5
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2022-03-18 19:21:32 -0300

    Programa pronto
    Ajustes: 
    -adição da biblioteca sympy
    -o usuário pode digitar a função para encontrar a raiz no intervalo
    -precisão de 4 para 5 casas decimais

--- bissecao.py
+++ bissecao.py
@@ -1,45 +1,46 @@
 #Objetivo: Fazer um programa que resolva o algoritmo da bisseção
-#Autor: Heloísa Rades de Souza      Data:16/03/2022
+#Autor: Heloísa Rades de Souza      Data:18/03/2022
 
 #importar bibliotecas
-#import numpy as np
-import math
+import sympy as sp
+from sympy.parsing.sympy_parser import parse_expr
 
 print('''Esta é uma calculadora de raízes pelo método da bisseção
-Sua precisão é de 4 casas decimais''')
+Sua precisão é de 5 casas decimais
+Digite seguindo o modelo: x² -2x + 3  -->  x**2 - 2*x +3\n''')
 
 
 #Ler intervalo e erro informados pelo usuário
+funcao = input('Digite a função: ').lower()
 a = float(input('Digite o início do intervalo: '))
 b = float(input('Digite o final do intervalo: '))
 erro = float(input('Digite o erro: '))
 
-funcao = '{x**2 - 3}'
+x = sp.symbols('x')
+
 #Função para calcular x-barra (média)
-def media(a,b):
+def Media(a,b):
     med = (a+b)/2
     return med
 
 #Ler e calcular a função
-def f(x):
-    return x**2 - 3
+def f(num, exp):
+    resultado = parse_expr(exp)   #Transforma a string informada pelo usuário em uma expressão "resolvível"
+    return resultado.subs(x, num)
 
-#Calcular número de iterações  --  não está funcionando
-#n_iteracoes = (np.log(a-b) - np.log(erro))/np.log(2)
-#k = np.ceil(n_iteracoes)
-#print(f'O número mínimo de iterações calculado foi {k}')
 
-if f(a)*f(b) < 0:
-    while True:
-       x0 = media(a,b)
+if f(a, funcao)*f(b, funcao) <= 0: #Confere a principal definição para haver uma raíz no intervalo
+    while True:   #Loop de repetição infinita
+       x0 = Media(a,b)
        
-       print(f'''a = {a:.4f} | b = {b:.4f} | x = {x0:.4f} | f(a) = {f(a):.4f} | f(x) = {f(x0):.4f} | sinal = {f(a)*f(b):.4f} | E = {(b-a):.4f}''')
+       #Saída apresentando os valores de cada variável e valor de função com 5 casas decimais
+       print(f'''a = {a:.5f} | b = {b:.5f} | x = {x0:.5f} | f(a) = {f(a, funcao):.5f} | f(x) = {f(x0, funcao):.5f} | sinal = {f(a, funcao)*f(b, funcao):.5f} | E = {(b-a):.5f}''')
        
-       if (b-a) < erro:
-           print(f'{x0:.4f} é a raiz da função')
+       if (b-a) < erro:  #Confere se a diferença intervalo é menor que o erro informado e finaliza o programa
+           print(f'{x0:.5f} é a raiz da função')
            break
-       else:
-           if f(a)*f(x0) > 0:
+       else:  #Caso não seja menor, o valor da média é atribuido ao começo ou fim do intervalo, dependendo do valor identificado como "sinal"
+           if f(a, funcao)*f(x0, funcao) > 0: 
                a = x0
            else:
                b = x0
